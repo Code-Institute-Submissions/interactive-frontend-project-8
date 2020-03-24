cf07b4c6d40edc2a6157d90049cc05733f15724b
alicia rawlings <lilyrawlings123@gmail.com>
2020-03-24 14:19:58 +0000

    game var , cleargame & startgame func made

---
+++ script/script.js
@@ -0,0 +1,22 @@
+var game ={
+    started:false,
+    colors: ["#51ff0d" , "#4deeea" , "#ffe700"],
+    currentGame: [],
+    playerMoves: [],
+
+}
+
+function clearGame(){
+    started= true,
+    currentGame = [],
+    playerMove = 0;
+
+
+}
+
+function startGame() {
+    clearGame();
+    started = true;
+
+
+}
