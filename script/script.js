7d20b4f4bc0cbcc813910bbb4bb8b4e629d5540c
alicia rawlings <lilyrawlings123@gmail.com>
2020-03-25 14:56:07 +0000

    flip card onlick

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 var game = {
     started: false,
-    colors: ["#51ff0d" , "#4deeea" , "#ffe700"],
+    showing: false,
+    color: ["#51ff0d" , "#4deeea" , "#ffe700"],
     currentGame: [],
     playerMoves: [],
     playerMove: 0,
@@ -10,7 +11,8 @@ var game = {
 const elements = {
     gameContainer: $('#game-container'),
     gameMenu: $('#game-menu'),
-    
+    tiles: document.querySelectorAll('.tile'),
+  
 }
 
 function clearGame() {
@@ -25,32 +27,74 @@ function startGame() {
 
     
 
-    
+   
     elements.gameMenu.hide()
     elements.gameContainer.show()
 
 
-
-  
+    nextPattern()
+    setTimeout(showPattern, 1500);
 }
+
+function nextPattern() {
+    var nextTileId = Math.floor(Math.random() * 4);
+    while (nextTileId == game.currentGame[game.currentGame.length - 1]) {
+        nextTileId = Math.floor(Math.random() * 4);
+    }
+    game.currentGame.push(nextTileId);
+    game.playerMove = 0;
+}
+
 
 
 function exitGame() {
     clearGame()
-  
+   
+    
 }
 
 
+function showPattern() {
+    
+    game.showing = true;
+  
 
-   
+    var move = 0;
+    
+    flipTile(elements.tiles[game.currentGame[move]])
+    move++;
 
+    
 
-                
+}
+
+  
                
 
 
 
+function flipTile(tile) {
+    
+    unflipOtherTiles(tile)
+    
+    
+    tile.classList.add('flip-card-onclick');
+    
+    
+    setTimeout(function() {
+        tile.classList.remove('flip-card-onclick')
+    }, 1000)
+}
 
+function unflipOtherTiles(currentTile) {
+    
+    elements.tiles.forEach(function(tile) {
+        
+        if (tile != currentTile) {
+            tile.classList.remove('flip-card-onclick')
+        }
+    })
+}
 
 // 1. start game
 // 2. first pattern
