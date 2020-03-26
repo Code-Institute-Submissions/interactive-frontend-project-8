83543a07f3b1209f8f53d7a7457ae0fec97f6417
alicia rawlings <lilyrawlings123@gmail.com>
2020-03-26 21:35:31 +0000

    wait & go text, game checking match correct

--- script/script.js
+++ script/script.js
@@ -12,8 +12,12 @@ const elements = {
     gameContainer: $('#game-container'),
     gameMenu: $('#game-menu'),
     tiles: document.querySelectorAll('.tile'),
-  
+    
+    waitText: $('#wait-text'),
+    goText: $('#go-text')
 }
+  
+
 
 function clearGame() {
     game.started = false;
@@ -25,10 +29,8 @@ function startGame() {
     clearGame();
     game.started = true;
 
-    
-
-   
-    elements.gameMenu.hide()
+    elements.goText.hide()
+     elements.gameMenu.hide()
     elements.gameContainer.show()
 
 
@@ -53,23 +55,64 @@ function exitGame() {
     
 }
 
-
+// show user the pattern they have to repeat
 function showPattern() {
-    
+    // set game status to showing pattern
     game.showing = true;
-  
+    elements.goText.hide();
+    elements.waitText.show();
 
     var move = 0;
-    
+    // show first tile in the pattern
     flipTile(elements.tiles[game.currentGame[move]])
     move++;
 
-    
+    // every 1 second show the next tile in the pattern
+    var interval = setInterval(function() {
+        // when the end of pattern is reached destroy the interval and disable the showing status
+        if (move == game.currentGame.length) {
+            game.showing = false;
+            elements.goText.show();
+            elements.waitText.hide();
 
+            setTimeout(function() {
+                elements.goText.hide()
+            }, 2000);
+
+            clearInterval(interval);
+        } else {
+            flipTile(elements.tiles[game.currentGame[move]]);
+            move++;
+            
+        }
+    }, 1000);
 }
 
+
+
   
+function tileClicked(tile) {
+    console.dir(tile)
+    // only allow clicking on tiles when game is started and game is not showing pattern
+    if (!game.showing && game.started && !tile.classList.contains('flip-card-onclick')) {
+
+        flipTile(tile);
+
+        
+            // check if current move (tile clicked) matches the tile in the generated pattern
+            if (parseInt(tile.id) == game.currentGame[game.playerMove]) {
+                // increase the pattern pointer
+                game.playerMove++;
+                
+                
+
+                
                
+            
+            }
+        }
+    }
+            
 
 
 
